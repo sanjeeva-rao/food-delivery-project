cfb7ec129cde284bf2e8ec32dd5deca22c189458
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-08-17 09:00:36 +0530

    created outlet (header and Body)

--- namaste-food-delivery/src/App.js
+++ namaste-food-delivery/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header';
+import Body from "./Components/Body"
 
 function App() {
   return (
     <div className="">
-      Namaste React
+      <Header />
+      <Body />
     </div>
   );
 }
