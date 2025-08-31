eb6637b8836d1447101682fb2bef8d4315ce5977
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-08-31 10:05:53 +0530

    Added Search functionality to serch the restaurants by name

--- namaste-food-delivery/src/Components/Body.js
+++ namaste-food-delivery/src/Components/Body.js
@@ -3,14 +3,32 @@ import { useEffect, useState } from "react";
 import {ResCard} from "./ResCard"
 const Body = () => {
     const [restaurantData, setRestaurantData] = useState([]);
+    const [defaultResData, setDefaultResData] = useState([]);
+    const [searchText, setSearchText] = useState("");
+
     useEffect(()=>{
         setRestaurantData(resData);
+        setDefaultResData(resData);
     },[])
+
+    const filterSearchRestaurants = () => {
+        var filetrRes = defaultResData.filter(res => res.info.name.toUpperCase().includes(searchText.toUpperCase()));
+        setRestaurantData(filetrRes);
+    }
+
     return (
-        <div className="flex flex-wrap">
+        <div>
+            <div className="px-2 lg:px-8 pt-4">
+                <input type="text" placeholder="Search Restaurants" className="px-4 py-1 border border-black" value={searchText} 
+                    onChange={(e)=>setSearchText(e.target.value)}
+                />
+                <button className="bg-gray-400 px-2 py-1 rounded-lg mx-8" onClick={filterSearchRestaurants}>Search</button>
+            </div>
+            <div className="flex flex-wrap">
             {
                 restaurantData.map((res)=> <ResCard resInfo = {res.info} key={res.info.id} />)
             }
+        </div>
         </div>
     )
 }
