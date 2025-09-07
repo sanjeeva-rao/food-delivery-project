3eca1f7993c904bda9fa80dac54a2418c05e067b
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-09-07 11:19:08 +0530

    - Update Menu card - Added veg icons for veg restaurats and items

--- namaste-food-delivery/src/Components/MenuCards.js
+++ namaste-food-delivery/src/Components/MenuCards.js
@@ -4,22 +4,22 @@ import { useState } from "react";
 const MenuCards = ({resCard}) => {
     const [showItems,setShowItems] = useState(false);
     return <div className="px-4 shadow-xl py-4 w-[90%] mx-12">
-        <div className="font-bold text-lg flex cursor-pointer" onClick={()=>setShowItems(!showItems)}>
-            <div className="w-[99%]">{resCard.title}</div>
+        <div className="font-bold text-lg flex cursor-pointer pt-4" onClick={()=>setShowItems(!showItems)}>
+            <div className="w-[99%]">{resCard.title + " (" + resCard.itemCards.length + ")"}</div>
             <img className="h-6 w-6" src={showItems ? collapseIcon : expandIcon} alt="expand-collapse"/>
         </div>
         { showItems &&
             resCard.itemCards?.map(card => <div className="py-2">
                 <div className="flex">
                     <div className="w-[92%]">
-                        <div className="font-semibold">{card.card.info.name}</div>
+                        <div className="font-semibold">{card.card.info.isVeg ? "🟢 " +card.card.info.name : card.card.info.name}</div>
                         <div>₹ {card.card.info.price/100}</div>
                         <div>{card.card.info.description}</div>
                         <div>{card.card.info.ratings.aggregatedRating.rating} Rating</div>
                     </div>
                     <div className="">
-                        <img className="h-32 w-32" alt="img" src={cloudImgId + card.card.info.imageId} />
-                        <div className="px-1 py-2 text-center bg-slate-300 text-green-700 rounded-lg my-1">Add</div>
+                        {card.card.info.imageId && <img className="h-32 w-32" alt="img" src={cloudImgId + card.card.info.imageId} />}
+                        <div className="px-1 py-2 text-center bg-slate-300 text-green-700 rounded-lg my-1 w-32">Add</div>
                     </div>
                 </div>
                 <br></br>
--- namaste-food-delivery/src/Components/ResMenu.js
+++ namaste-food-delivery/src/Components/ResMenu.js
@@ -5,6 +5,7 @@ import ShimmerUi from "./ShimmerUi";
 import MenuCards from "./MenuCards";
 const ResMenu = () => {
     const [menuData, setMenuData] = useState(null);
+    const [resDetails, setResDetails] = useState(null);
     useEffect(()=>{
         fetchMenuItems();
     },[])
@@ -14,8 +15,18 @@ const ResMenu = () => {
         const json = await data.json();
         var resNeeded = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(resCard => resCard.card.card.title && resCard.card.card.itemCards);
         setMenuData(resNeeded);
+        setResDetails(json?.data?.cards[2]?.card?.card)
     }
-    return menuData ? menuData.map(resCard => <MenuCards resCard = {resCard.card.card} key={resCard.card.card.title}/>) 
+    return menuData && resDetails ? <div>
+        <div className="ml-[4%] my-2 px-4 py-4 shadow-2xl w-[90%]">
+            <div className="font-bold text-xl">{resDetails.info.name}</div>
+            <div>{resDetails.info.avgRating} (Rating) {resDetails.info.costForTwoMessage}</div>
+            <div>{resDetails.info.cuisines.join(",")}</div>
+            <div>{resDetails.info.locality}</div>
+        </div>
+        {resDetails.info.veg && <div className="text-green-500 bg-gray-300 w-48 px-4 py-1 rounded-lg ml-[4%] mt-4">Pure Veg Restaurant</div>}
+        { menuData.map(resCard => <MenuCards resCard = {resCard.card.card} key={resCard.card.card.title}/>) }
+    </div>
      : <ShimmerUi />
 }
 export default ResMenu;
