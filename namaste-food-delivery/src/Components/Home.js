d7f4d2a47bea95cd50e76cc1bcd7f72dedd78499
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-09-10 20:27:04 +0530

    - Added lazy loading - Added seperate bundle for Groceries

--- namaste-food-delivery/src/Components/Home.js
+++ namaste-food-delivery/src/Components/Home.js
@@ -1,13 +1,16 @@
+import { lazy, Suspense } from "react";
 import FoodHome from "./FoodHome";
-import GroceriesHome from "./GrocerriesHome";
 
 const Home = () => {
+    const GroceriesHome = lazy(()=>import("./GrocerriesHome"))
     return <div className="bg-orange-400 h-full">
         <div className="py-10">
             <div className="text-white text-center font-bold text-2xl">Order Food & Groceries. Discover best Restaurants</div>
             <div className="flex mx-4 lg:mx-[25%] py-16">
                 <FoodHome />
-                <GroceriesHome />
+                <Suspense>
+                    <GroceriesHome />
+                </Suspense>
             </div>
         </div>
     </div>
