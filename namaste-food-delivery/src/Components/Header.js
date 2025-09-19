a8d3121e0ff273ca192d040389ca443394f17058
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-09-19 21:48:10 +0530

    Added Home button for restaurants

--- namaste-food-delivery/src/Components/Header.js
+++ namaste-food-delivery/src/Components/Header.js
@@ -1,14 +1,18 @@
 import { cartIcon, logo, profile } from "../utilities/constants";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import ResListContext from "../utilities/ResListContext";
 
 const Header = () => {
     const {cartItems} = useContext(ResListContext);
+    const navigate = useNavigate()
     return (
         <div className="flex justify-between p-4 bg-gray-200">
             <div>
                 <Link to={"/"}><img className="h-14 lg:h-14 w-14 lg:w-14" alt="logo" src={logo}/></Link>
+            </div>
+            <div className="bg-orange-500 px-4 rounded-lg h-10 py-2 text-white cursor-pointer ml-[70%] my-2" onClick={()=>navigate("/foodDelivery")}>
+                Restaurants
             </div>
             <Link to={"/foodDelivery/cart"}>
                 <div className="flex justify-between">
