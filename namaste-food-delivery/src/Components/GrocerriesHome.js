a932e39e8007cb3bd925b9b7f28c325b2882cfc3
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-09-20 10:16:35 +0530

    Update GUI changes

--- namaste-food-delivery/src/Components/GrocerriesHome.js
+++ namaste-food-delivery/src/Components/GrocerriesHome.js
@@ -2,11 +2,10 @@ import { useNavigate } from "react-router-dom";
 
 const GroceriesHome = () => {
     const navigate = useNavigate();
-    return <div className="h-[250px] w-[250px] lg:h-[350px] lg:w-[350px] bg-white mr-10 rounded-lg cursor-pointer" onClick={()=>{navigate("/grocery")}}>
+    return <div className="h-[250px] w-[250px] lg:h-[350px] lg:w-[350px] bg-white rounded-lg cursor-pointer" onClick={()=>navigate("/grocery")}>
         <div className="text-center py-2">
-            <div className="font-bold text-xl lg:text-2xl">Insta Mart</div>
-            <div className="font-bold text-xl lg:text-2xl">Instant Grocery</div>
-            <img alt="logo" src="https://tse2.mm.bing.net/th/id/OIP.1h1hiRSUtDsGd2wow6BMjgHaE7?pid=Api&P=0&h=180" className="h-28 w-28 lg:h-40 lg:w-40 mx-[25%] my-4 lg:my-8"/>
+            <div className="font-bold text-sm lg:text-2xl">Insta Mart From Instant Grocery</div>
+            <img alt="logo" src="https://tse2.mm.bing.net/th/id/OIP.1h1hiRSUtDsGd2wow6BMjgHaE7?pid=Api&P=0&h=180" className="h-20 w-20 lg:h-40 lg:w-40 mx-[20%] lg:mx-[20%] my-4 lg:my-8"/>
             <div>➡️</div>
         </div>
     </div>
--- namaste-food-delivery/src/Components/Home.js
+++ namaste-food-delivery/src/Components/Home.js
@@ -3,7 +3,7 @@ import FoodHome from "./FoodHome";
 
 const Home = () => {
     const GroceriesHome = lazy(()=>import("./GrocerriesHome"))
-    return <div className="bg-orange-400 h-full">
+    return <div className="bg-orange-400">
         <div className="py-10">
             <div className="text-white text-center font-bold text-2xl">Order Food & Groceries. Discover best Restaurants</div>
             <div className="flex mx-4 lg:mx-[25%] py-16">
